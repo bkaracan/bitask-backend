df9aea5f6507988118f492fe59af706738b72dc5
bkaracan.home <burakkaracan94@gmail.com>
2024-12-09 09:14:04 +0300

    ++first initialization commit for remote repository

---
+++ src/main/java/com/bia/bitask_backend/BitaskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bia.bitask_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitaskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitaskBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bia/bitask_backend/BitaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bia.bitask_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitaskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
