06240e539ee583068fca0fddc4ff71e06870c01b
Burak Karacan <burakkaracan94@gmail.com>
2024-12-29 23:43:20 +0300

    ++payload changed for http response management

--- src/main/java/com/bia/bitask_backend/enumaration/core/ResponseEnum.java
+++ src/main/java/com/bia/bitask_backend/enumaration/core/ResponseEnum.java
@@ -1,23 +1,33 @@
 package com.bia.bitask_backend.enumaration.core;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
 public enum ResponseEnum {
-
-    OK(200,"OK", Boolean.TRUE),
-    BAD_REQUEST(400,"BAD_REQUEST", Boolean.FALSE),
-    UNAUTHORIZED(401,"UNAUTHORIZED", Boolean.FALSE),
-    FORBIDDEN(403,"FORBIDDEN", Boolean.FALSE),
-    NOT_FOUND(404,"NOT_FOUND", Boolean.FALSE),
-    ERROR(1000,"ERROR", Boolean.FALSE),
-    NOTIFICATION(1001,"NOTIFICATION", Boolean.FALSE),
-    INFO(1002,"INFO", Boolean.TRUE),
-    WARNING(100,"WARNING", Boolean.TRUE);
+    OK(200, "Success", Boolean.TRUE),
+    BAD_REQUEST(400, "Bad Request", Boolean.FALSE),
+    UNAUTHORIZED(401, "Unauthorized", Boolean.FALSE),
+    FORBIDDEN(403, "Forbidden", Boolean.FALSE),
+    NOT_FOUND(404, "Not Found", Boolean.FALSE),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error", Boolean.FALSE);
 
     private final Integer httpStatusCode;
     private final String description;
     private final Boolean isSuccess;
+
+    ResponseEnum(Integer httpStatusCode, String description, Boolean isSuccess) {
+        this.httpStatusCode = httpStatusCode;
+        this.description = description;
+        this.isSuccess = isSuccess;
+    }
+
+    public Integer getHttpStatusCode() {
+        return httpStatusCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Boolean getSuccess() {
+        return isSuccess;
+    }
 }
